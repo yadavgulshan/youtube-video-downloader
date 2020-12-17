128f8169de7f8e7772a34226369ab8b2c09673c3
Gulshan Yadav <gulshanyadav@localhost.localdomain>
2020-12-17 18:53:27 +0530

    Changed some required stuff to download video

--- testytdl.py
+++ testytdl.py
@@ -1,15 +1,17 @@
 import json
 from youtubesearchpython import SearchVideos
 import os
-name = input("Name of video to download.")
-search_link = f"https://www.youtube.com/search?q={name.replace(' ','%20')}"
+YesorNo = input("Do you have a link or you want me to search ?\n1. Yes I have a link\n2. No, I want to search the name and want to download the first video in query\n:")
+if (YesorNo == 1):
+    link = input("Please input your link: ")
+else:
+    name = input("Name of video to download: ")
+    search_link = f"https://www.youtube.com/search?q={name.replace(' ','%20')}"
+    search = SearchVideos(name, offset=1,
+                          mode="json", max_results=1)
+    result = search.result()
+    result = json.loads(result)
+    link = result["search_result"][0]["link"]
 
-
-# download part
-search = SearchVideos(name, offset=1,
-                      mode="json", max_results=1)
-
-result = search.result()
-result = json.loads(result)
-link = result["search_result"][0]["link"]
-os.system(f'youtube-dl -x --playlist-end 1 --audio-format flac {link}')
+os.system(f'youtube-dl -f best {link}')
+print("Video link: ", link)
